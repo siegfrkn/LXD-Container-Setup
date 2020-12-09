5cdd0150a6ab4016e68d1a3f4dfc9eb5658baf8a
siegfrkn <katrina.siegfried@gmail.com>
2020-12-08 19:50:48 -0700

    neaten up cleanup script

--- HW4_1/cleanup_capsule.sh
+++ HW4_1/cleanup_capsule.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
+echo "Stopping and removing hw4a...."
 lxc stop hw4a
 lxc delete hw4a
+echo " "
 
+echo "Stopping and removing hw4a...."
 lxc stop hw4b
 lxc delete hw4b
+echo " "
 
+echo "Delete the hw4_image...."
 lxc image delete hw4_image
+echo " "
+
+echo "Cleanup Complete!"
